78f567a2a7fe235a0ab22fb16bd9bfe907c2488b
unknown <codewithknr@outlook.com>
2025-07-12 20:26:00 +0530

    React skelton part 2

--- API/Program.cs
+++ API/Program.cs
@@ -10,9 +10,12 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("https://localhost:3000", "http://localhost:3000"));
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
