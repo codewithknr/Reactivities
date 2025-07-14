2d17c40f8c6cc98380849e573361909043fee4b8
unknown <codewithknr@outlook.com>
2025-07-14 16:58:00 +0530

    MediatR ip

--- API/ActivitiesController.cs
+++ API/ActivitiesController.cs
@@ -1,24 +1,26 @@
 using System;
+using Application.Activities.Queries;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API;
 
-public class ActivitiesController(AppDbContext context) : BaseApiController
+public class ActivitiesController(IMediator mediator) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await context.Activities.ToListAsync();
+        //Using Implement ORM Layer
+        //return await context.Activities.ToListAsync();
+
+        //Using MediatR
+        return await mediator.Send(new GetActivityList.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        var activity = await context.Activities.FindAsync(id);
-        if (activity == null) return NotFound();
-        return activity;
+        return await mediator.Send(new GetActivityDetails.Query { Id = id });
     }
 }
